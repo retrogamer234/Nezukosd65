3ecc997122d6d8520df7e20b7fe29e2cc6512d40
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 02:01:25 +0530

    Update ass.js

--- Organs/commands/Nsfw/ass.js
+++ Organs/commands/Nsfw/ass.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*💦*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*Mitsu*`,
         buttons: buttons,
         headerType: 4
     }
